4736ce0935f0769c19be77bb6b63d44158708561
Joey Bream <jbream@ncsu.edu>
2021-10-29 18:52:55 -0400

    Create the DatabaseContext service

---
+++ Tech@HubAPI/Tech@HubAPI/Models/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tech_HubAPI.Models
+{
+	public partial class DatabaseContext : DbContext
+	{
+		private string _connectionString;
+
+		public DatabaseContext(DbContextOptions<DatabaseContext> options)
+			: base(options)
+		{ }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseMySql(_connectionString, ServerVersion.AutoDetect(_connectionString));
+			}
+		}
+	}
+}
--- Tech@HubAPI/Tech@HubAPI/Startup.cs
+++ Tech@HubAPI/Tech@HubAPI/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -12,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tech_HubAPI.Models;
 
 namespace Tech_HubAPI
 {
@@ -27,6 +29,12 @@ namespace Tech_HubAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			string connectionString = "";
+
+			services.AddDbContext<DatabaseContext>(options =>
+			{
+				options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+			});
 
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
