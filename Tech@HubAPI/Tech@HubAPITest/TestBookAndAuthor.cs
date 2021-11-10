9829a1890abadf1330fbb3e99c43a8d5da4d8ecf
Joey Bream <jbream@ncsu.edu>
2021-11-10 17:15:09 -0500

    Setup automatic database management for testing

---
+++ Tech@HubAPI/Tech@HubAPITest/TestBookAndAuthor.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tech_HubAPI.Models;
+using Xunit;
+
+namespace Tech_HubAPITest
+{
+	public class TestBookAndAuthor : DatabaseTest
+	{
+		[Fact]
+		public void TestCreateAndRetrieveAuthor()
+		{
+			var author = new Author("Ken");
+			DbContext.Authors.Add(author);
+			DbContext.SaveChanges();
+
+			author = DbContext.Authors
+				.Where(a => a.Name == "Ken")
+				.First();
+
+			author.Name.Should().Be("Ken");
+		}
+
+		[Fact]
+		public void TestCreateAndRetrieveBook()
+		{
+			var author = new Author("Ken");
+			DbContext.Authors.Add(author);
+			DbContext.SaveChanges();
+
+			var book1 = new Book("Harry Potter", DateTime.Now, author.Id);
+			var book2 = new Book("Star Wars", DateTime.Now, author.Id);
+			DbContext.Books.Add(book1);
+			DbContext.Books.Add(book2);
+			DbContext.SaveChanges();
+
+			author = DbContext.Authors
+				.Where(a => a.Name == "Ken")
+				.First();
+
+			author.Books.Count.Should().Be(2);
+			author.Books[0].Title.Should().Be("Harry Potter");
+			author.Books[1].Title.Should().Be("Star Wars");
+		}
+
+		[Fact]
+		public void TestDuplicateBookName()
+		{
+			var author = new Author("Ken");
+			DbContext.Authors.Add(author);
+			DbContext.SaveChanges();
+
+			var book1 = new Book("Harry Potter", DateTime.Now, author.Id);
+			var book2 = new Book("Harry Potter", DateTime.Now, author.Id);
+			DbContext.Books.Add(book1);
+			DbContext.Books.Add(book2);
+			
+			Action save = () => DbContext.SaveChanges();
+			save.Should().Throw<DbUpdateException>();
+		}
+	}
+}
