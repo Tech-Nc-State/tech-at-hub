44f8c1e2658a675b31596732057c113db2f03403
Joey Bream <jbream@ncsu.edu>
2021-11-12 09:50:13 -0500

    Fix issue with running tests in parallel

--- Tech@HubAPI/Tech@HubAPITest/DatabaseTest.cs
+++ Tech@HubAPI/Tech@HubAPITest/DatabaseTest.cs
@@ -13,13 +13,18 @@ namespace Tech_HubAPITest
 				.AddJsonFile("developersecrets.json")
 				.Build();
 			string connectionString = config.GetConnectionString("MySqlDatabase");
+
+			// xUnit seems to try to run seperate test files in parallel. This isn't a great solution,
+			// but we generate a random database name for each test to ensure they don't collide
+			connectionString = connectionString
+				.Replace("tech-at-hub-test", "tech-at-hub-" + new Random().NextDouble());
+
 			var options = new DbContextOptionsBuilder<DatabaseContext>()
 				.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
 				.Options;
 
 			DbContext = new DatabaseContext(options);
 			DbContext.Database.EnsureCreated();
-            
 		}
 
 		protected DatabaseContext DbContext { get; private set;}
