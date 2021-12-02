df853fd02759839dd1206991401b70fc858b271e
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-12-01 23:23:13 -0500

    Added unit tests for Execute.cs

---
+++ Tech@HubAPI/Tech@HubAPITest/ExecuteTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tech_HubAPI.Models;
+using Xunit;
+using Tech_HubAPI; // This lets me call Execute.cs (same namespace)
+
+
+namespace Tech_HubAPITest
+{
+    public class ExecuteTest
+    {
+        [Fact]
+        public void TestEcho()
+        {
+            string output = Execute.ExecuteProcess("echo", "hi");
+            output.Should().Be("hi");
+
+        }
+
+        [Fact]
+        public void TestNonexistantProgram()
+        {
+            string output = Execute.ExecuteProcess("jhgetrhb", "bad_argument");
+            output.Should().Be("The system cannot find the file specified.");
+        }
+
+    }
+}
