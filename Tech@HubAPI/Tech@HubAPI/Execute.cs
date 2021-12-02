a529f4e53a9580e479c40c8783328175d6244685
Heckin-Doggo <43428231+Heckin-Doggo@users.noreply.github.com>
2021-12-02 00:13:32 -0500

    Added new tests to ExecuteTest

--- Tech@HubAPI/Tech@HubAPI/Execute.cs
+++ Tech@HubAPI/Tech@HubAPI/Execute.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.ComponentModel;
+using System.IO;
 
 namespace Tech_HubAPI
 {
@@ -16,13 +17,16 @@ namespace Tech_HubAPI
             // setup arguments to single string
             string argsString = "";
             int maxArgsIndex = args.Length - 1;
-            for (int i = 0; i < maxArgsIndex; i++)
+            if (args.Length != 0)
             {
-                // add spaces
-                argsString += args[i] + " ";
+                for (int i = 0; i < maxArgsIndex; i++)
+                {
+                    // add spaces
+                    argsString += args[i] + " ";
+                }
+                // last one is unspaced
+                argsString += args[maxArgsIndex];
             }
-            // last one is unspaced
-            argsString += args[maxArgsIndex];
 
             // yoinked from MS Docs
             try
@@ -31,8 +35,10 @@ namespace Tech_HubAPI
                 {
                     //myProcess.StartInfo.UseShellExecute = false;
                     // You can start any process, HelloWorld is a do-nothing example.
+
                     myProcess.StartInfo.FileName = programName;
                     myProcess.StartInfo.Arguments = argsString;
+                    myProcess.StartInfo.RedirectStandardOutput = true;
 
                     myProcess.Start();
 
@@ -43,6 +49,7 @@ namespace Tech_HubAPI
             catch (Exception e)
             {
                 // might as well print the error i guess.
+                // actually this turned out to be very useful for unit tests so yeah.
                 output = e.Message;
             }
             // end yoink
@@ -50,7 +57,6 @@ namespace Tech_HubAPI
             return output;
         }
 
-
-
     }
 }
+
--- Tech@HubAPI/Tech@HubAPITest/ExecuteTest.cs
+++ Tech@HubAPI/Tech@HubAPITest/ExecuteTest.cs
@@ -15,10 +15,34 @@ namespace Tech_HubAPITest
     public class ExecuteTest
     {
         [Fact]
-        public void TestEcho()
+        public void TestEchoFullPathPartialString()
+        {
+            string output = Execute.ExecuteProcess("C:\\Program Files\\Git\\usr\\bin\\echo.exe", "hi");
+            output.Should().Contain("hi");
+
+        }
+
+        [Fact]
+        public void TestEchoFullPathExactString()
+        {
+            string output = Execute.ExecuteProcess("C:\\Program Files\\Git\\usr\\bin\\echo.exe", "hi");
+            output.Should().Be("hi");
+
+        }
+
+        [Fact]
+        public void TestEchoCommand()
         {
             string output = Execute.ExecuteProcess("echo", "hi");
             output.Should().Be("hi");
+
+        }
+
+        [Fact]
+        public void TestEchoCommandPartialString()
+        {
+            string output = Execute.ExecuteProcess("echo", "hi");
+            output.Should().Contain("hi");
 
         }
 
@@ -29,5 +53,24 @@ namespace Tech_HubAPITest
             output.Should().Be("The system cannot find the file specified.");
         }
 
+        [Fact]
+        public void TestFullPathBatchFileExactString()
+        {
+            // The purpose of this test was to see if the process was failing because echo is an "internal command" without a stored exe.
+            // The .bat file just calls "echo Hello World!". 
+            string output = Execute.ExecuteProcess("C:\\Users\\neil8\\OneDrive\\Documents\\TechAtState\\tech-at-hub\\Tech@HubAPI\\Tech@HubAPITest\\testExecute.bat");
+            output.Should().Contain("Hello World!");
+        }
+
+        [Fact]
+        public void TestFullPathBatchFilePartialString()
+        {
+            // The difference between this test and the above is that the above doesn't account for whitespace
+            // whereas this one just ignores the whitespace. IDK where the whitespace comes from.
+            string output = Execute.ExecuteProcess("C:\\Users\\neil8\\OneDrive\\Documents\\TechAtState\\tech-at-hub\\Tech@HubAPI\\Tech@HubAPITest\\testExecute.bat");
+            output.Should().Contain("Hello World!");
+        }
+
+        
     }
 }
