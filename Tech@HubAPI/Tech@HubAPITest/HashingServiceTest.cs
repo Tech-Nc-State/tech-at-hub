563ebfa3bd8158cdef32c011b907a846c4e3545e
Joey Bream <jbream@ncsu.edu>
2021-12-19 15:40:25 -0500

    Changed test to use Theory attribute

--- Tech@HubAPI/Tech@HubAPITest/HashingServiceTest.cs
+++ Tech@HubAPI/Tech@HubAPITest/HashingServiceTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 using FluentAssertions;
 using Tech_HubAPI.Services;
 
@@ -36,28 +37,35 @@ namespace Tech_HubAPITest
             salt3.Should().NotEqual(salt1);
         }
 
-        [Fact]
-        public void HashPasswordTest()
+        [Theory]
+        [InlineData(new object[] { "password", "dd4ad1d3d6f001be8fc87b7d4789d69d58e7229b71c0aa8387dc78b24ad24b0c01ea44e84c15f92826989f9fd14c5032cc409c000b344e78dd1eca4b054a16af" })]
+        [InlineData(new object[] { "Wolfpack123!", "5199f92bca07852ec548e211b0f3886b7f11d7aed3078ca3f9525a3b539ff014fa370a4ecd52e471bece65e6fa61734b02e818ebaf379788fa61f4577d0f5664" })]
+        [InlineData(new object[] { "Very_Long*Password(&^%WITH+SPEciAL@!Characters", "f016990812e25660f1e867e0e7aabf93bf3795d56c49da8edf7d7762e8903bf6bef95e5a877b02db997a5915b00cc24eda4a44fe2b63bde9f8d4040165f5c0d3" })]
+        public void HashPasswordTest(string plainText, string expectedHash)
         {
-            byte[] salt = Enumerable.Repeat<byte>(0xFF, 32).ToArray();
-
-
-            String password1 = "password";
-            String password2 = "Wolfpack123!";
-            String password3 = "Very_Long*Password(&^%WITH+SPEciAL@!Characters";
-
             // Used https://emn178.github.io/online-tools/sha512.html to get these SHA512 byte sequences
-            String actualHashedPassword1 = "b6-41-61-ad-24-d1-48-8d-02-82-82-76-04-a2-b4-68-c8-42-e6-cd-2b-21-9f-12-0a-c1-e3-4d-eb-cf-ff-20-66-b0-5f-8c-7a-1a-71-e5-ce-88-46-3c-36-ba-b0-1a-a8-81-4e-1b-88-d5-b4-aa-ad-bd-c3-62-3e-97-e9-1c";
-            String actualHashedPassword2 = "c8-f3-19-af-e8-23-10-2e-80-29-36-2c-71-c0-13-73-d7-97-eb-39-a5-59-dd-68-eb-24-d8-c0-83-ee-1f-f7-8e-a3-7b-64-b4-57-95-25-33-c2-64-01-ac-5b-b3-5f-64-dd-bd-6e-bb-87-cf-a2-57-6f-5e-ef-1a-98-5b-5c";
-            String actualHashedPassword3 = "68-74-de-b2-1e-c6-cb-d6-b0-3d-b3-a0-db-9b-7a-41-64-f9-5f-49-7a-22-85-d7-b7-3f-6c-d2-f6-55-68-bc-27-ad-7f-d7-01-3f-1a-9e-05-59-39-d2-4b-70-97-1f-dd-97-bb-c1-c7-4e-2d-3e-74-9c-58-df-27-eb-88-15";
 
-            byte[] hashedPassword1 = service.HashPassword(password1, salt);
-            byte[] hashedPassword2 = service.HashPassword(password2, salt);
-            byte[] hashedPassword3 = service.HashPassword(password3, salt);
+            byte[] salt = Enumerable.Repeat((byte)'a', 32).ToArray();
 
-            actualHashedPassword1.Should().BeEquivalentTo(BitConverter.ToString(hashedPassword1));
-            actualHashedPassword2.Should().BeEquivalentTo(BitConverter.ToString(hashedPassword2));
-            actualHashedPassword3.Should().BeEquivalentTo(BitConverter.ToString(hashedPassword3));
+            byte[] hashedPassword = service.HashPassword(plainText, salt);
+            string hashedPasswordText = BitConverter.ToString(hashedPassword);
+
+            hashedPasswordText.Should().BeEquivalentTo(InsertDashes(expectedHash));
+        }
+
+        private string InsertDashes(string hex)
+        {
+            var newHex = new StringBuilder();
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                newHex.Append(hex.Substring(i, 2));
+                newHex.Append('-');
+            }
+
+            // remove the trailing dash
+            newHex.Remove(newHex.Length - 1, 1);
+
+            return newHex.ToString();
         }
     }
 }
