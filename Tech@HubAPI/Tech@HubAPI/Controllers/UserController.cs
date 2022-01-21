fcba6024fb064477de4b80db69ae609bec092714
Ziyad <ziyad.abdelaziz03@gmail.com>
2022-01-20 19:36:25 -0500

    Implemet getUser by ID

--- Tech@HubAPI/Tech@HubAPI/Controllers/UserController.cs
+++ Tech@HubAPI/Tech@HubAPI/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
 using Tech_HubAPI.Models;
 
 namespace Tech_HubAPI.Controllers
@@ -15,5 +16,26 @@ namespace Tech_HubAPI.Controllers
 		{
 			_dbContext = dbContext;
 		}
+		
+		[HttpGet]
+		[Route("get/{ID}")]
+		public User GetUser(int ID) 
+        {
+			var user = _dbContext.Users.Where(u => u.Id == ID).FirstOrDefault();
+
+            user.Password = null;
+			user.Age = 0;
+			user.Email = null;
+			user.Salt = null;
+
+            if (user == null)
+            {
+				return NotFound();
+            }
+			else
+            {
+				return user;
+            }
+        }
 	}
 }
