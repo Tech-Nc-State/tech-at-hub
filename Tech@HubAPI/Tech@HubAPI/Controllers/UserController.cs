b808c986a6ea778217914bba8a003b0fd23884c9
jcmaggio <jcmaggio@ncsu.edu>
2022-01-20 19:49:44 -0500

    Created SignUp endpont in UserController

--- Tech@HubAPI/Tech@HubAPI/Controllers/UserController.cs
+++ Tech@HubAPI/Tech@HubAPI/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Linq;
+using Tech_HubAPI.Services;
 using Tech_HubAPI.Models;
 
 namespace Tech_HubAPI.Controllers
@@ -10,10 +12,25 @@ namespace Tech_HubAPI.Controllers
 	public class UserController : ControllerBase
 	{
 		private readonly DatabaseContext _dbContext;
+		private readonly HashingService _hashingService;
 
-		public UserController(DatabaseContext dbContext)
+		public UserController(DatabaseContext dbContext, HashingService hashingService)
 		{
 			_dbContext = dbContext;
+			_hashingService = hashingService;
 		}
+
+		[HttpPost]
+		public User SignUp(SignUpForm form)
+        {
+			IQueryable query = _dbContext.Users.Where(u => u.Username == form.username);
+			if (query != null)
+            {
+				throw new Exception();
+            }
+			byte[] salt = _hashingService.GetSalt();
+			byte[] hashedPassword = _hashingService.HashPassword(form.password, salt);
+			return new User(form.username, hashedPassword, salt, form.email, form.firstName, form.lastName, form.age, null, null);
+        }
 	}
 }
---
+++ Tech@HubAPI/Tech@HubAPI/Forms/SignUpForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class SignUpForm
+{
+
+	public SignUpForm(String username, String password, String email, String birthDate, String firstName, String lastName, int age)
+	{
+		
+	}
+
+	public String firstName { get; set; }
+	public String lastName { get; set; }
+	public String username { get; set; }
+	public String password { get; set; }
+	public String email { get; set; }
+	public String birthDate { get; set; }
+	public uint age { get; set; }
+
+}
