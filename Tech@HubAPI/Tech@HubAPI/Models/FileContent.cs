6c0dfaf870d650df6f9ef747fc5535217930a606
mkmcary <mkmcary@gmail.com>
2022-02-28 18:24:47 -0500

    Changes to get file contents in GitService.

---
+++ Tech@HubAPI/Tech@HubAPI/Models/FileContent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tech_HubAPI.Models
+{
+    public class FileContent
+    {
+        /// <summary>
+        /// The name of the file.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The size of the file in bytes.
+        /// </summary>
+        public long Size { get; set; }
+
+        /// <summary>
+        /// The Contents of the file.
+        /// TODO: Consider changing the type of contents to handle other file types?
+        /// </summary>
+        public string Contents { get; set; }
+        //TODO: Add reference to the last commit on this file
+        // public Commit LastCommit {get; set;}
+    }
+}
