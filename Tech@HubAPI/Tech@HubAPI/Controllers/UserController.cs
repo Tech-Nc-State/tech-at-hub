e5e622fe5e613eb1bbca2f5138ab2baea7d8da9f
Michael Costello <mwcostel@ncsu.edu>
2022-09-20 20:40:41 -0400

    Developed two endpoint for uploading and retrieving a profile picture.

--- Tech@HubAPI/Tech@HubAPI/Controllers/UserController.cs
+++ Tech@HubAPI/Tech@HubAPI/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tech_HubAPI.Models;
@@ -18,32 +20,36 @@ namespace Tech_HubAPI.Controllers
 
 		private readonly HashingService _hashingService;
 
+		private readonly string _defaultWorkingDirectory;
+
 		private string[] validImageExtensions = { ".png", ".jpg" };
 
-		public UserController(DatabaseContext dbContext, HashingService hashingService)
+		public UserController(DatabaseContext dbContext, HashingService hashingService, IConfiguration configuration)
 		{
 			_dbContext = dbContext;
 			_hashingService = hashingService;
+			_defaultWorkingDirectory = configuration["Environment:DefaultWorkingDirectory"].Replace("\\", "/");
 		}
 
 		[HttpPost]
 		[Route("uploadprofilepicture")]
 		//[Authorize]
 		public async Task<ActionResult> UploadProfilePicture(IFormFile file)
-        {
+		{
+
 			if (file == null || file.Length == 0)
-            {
+			{
 				return BadRequest("The file is empty.");
-            }
+			}
 
-			var path = Encoding.ASCII.GetString(_hashingService.HashFile(file));
+			var path = BitConverter.ToString(_hashingService.HashFile(file)).Replace("-", "");
 
-			using (var stream = System.IO.File.Create(path))
+			var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+			using (var stream = System.IO.File.Create(_defaultWorkingDirectory + "/profile_pictures/" + path + "." + ext))
 			{
 				await file.CopyToAsync(stream);
 			}
-
-			var ext = Path.GetExtension(file.FileName).ToLowerInvariant();
 
 			if (string.IsNullOrEmpty(ext) || Array.IndexOf(validImageExtensions, ext) == -1)
 			{
@@ -54,7 +60,39 @@ namespace Tech_HubAPI.Controllers
 
 			user.ProfilePicturePath = path;
 
-			return null;
+			return Ok();
 		}
+
+		[HttpGet]
+		[Route("getprofilepicture")]
+		public async Task<ActionResult>  GetProfilePicture(string username)
+        {
+			if (username == null || username.Length == 0)
+            {
+				return BadRequest("The user is empty.");
+            }
+
+			var user = _dbContext.Users.Where(u => u.Username == username).FirstOrDefault();
+
+			if(user == null)
+            {
+				return BadRequest("The username is not found.");
+            }
+
+			var path = user.ProfilePicturePath;
+
+			var ext = path.Substring(path.IndexOf("."));
+
+			var stream = System.IO.File.OpenRead(path);
+
+			var mime = "image/jpeg";
+
+			if(ext == "png")
+            {
+				mime = "image/png";
+            }
+
+			return new FileStreamResult(stream, mime);
+        }
 	}
 }
