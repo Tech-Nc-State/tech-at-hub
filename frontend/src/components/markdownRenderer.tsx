d956f35e2d5d965bcc11ca30467886fa607bed37
Jude Sproul <jnsproul@ncsu.edu>
2023-02-27 21:05:16 -0500

    hopefully this is what we wanted

--- frontend/src/components/markdownRenderer.tsx
+++ frontend/src/components/markdownRenderer.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from 'remark-gfm';
 
 interface markdownProps {
     text: string;
 }
 
 function renderMarkdown(props : markdownProps) {
-    return <ReactMarkdown>{props.text}</ReactMarkdown>
+    return <ReactMarkdown children = {props.text} remarkPlugins={[remarkGfm]}/>
 }
 
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import MarkdownRender from "./components/markdownRenderer";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import renderMarkdown from "./components/markdownRenderer";
+import { render } from "react-dom";
+
+
+const ting = `
+# h1 tag for fun
+A paragraph with *emphasis* and **strong importance**. 
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+`
 
 const router = createBrowserRouter([
   {
@@ -19,8 +34,8 @@ const router = createBrowserRouter([
       },
       {
           path: "/markdown-test",
-          element: <MarkdownRenderer markdown="##Hello##"/>
-      }
+          element: <MarkdownRender text = {ting}></MarkdownRender>,
+      },
     ],
   },
 ]);
