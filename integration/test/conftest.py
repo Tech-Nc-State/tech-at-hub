f261e2b57d1d309dbd59fb8f38bf4af0c74f5709
Your <you@example.com>
2023-04-05 22:20:47 -0400

    Update integration tests to use docker-compose file

--- integration/test/conftest.py
+++ integration/test/conftest.py
@@ -1,23 +1,10 @@
 import pytest
-import os
-import shutil
-import stat
-from util.containers import start_containers, stop_containers
-
-def onerror(func, path, exc_info):
-    if not os.access(path, os.W_OK):
-        os.chmod(path, stat.S_IWUSR)
-        func(path)
-    else:
-        raise
+from util.compose import compose_down, compose_up
+import time
 
 @pytest.fixture
-def containers():
-    git_folder = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "/git"
-    if not os.path.exists(git_folder):
-        os.mkdir(git_folder)
-    start_containers()
+def compose():
+    compose_up()
+    time.sleep(3)
     yield
-    stop_containers()
-    shutil.rmtree(git_folder, onerror=onerror)
-    os.mkdir(git_folder)
+    compose_down()
